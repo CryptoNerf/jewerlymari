ade8d5180dc0eed59f5798d2647f86f349827066
CryptoNerf <anuta1711_98@mail.ru>
2025-05-18 00:08:39 +0400

    fix bag

--- app/settings.py
+++ app/settings.py
@@ -26,9 +26,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-$2k37l30c_7f4gkp!(1cbf9i3(28%mfuir7m@miadx^%j+wdem'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['cryptonerf-jewerlymari-e0f8.twc1.net']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'cryptonerf-jewerlymari-e0f8.twc1.net']
 
 
 # Application definition
@@ -137,6 +137,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
+import os
 
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
@@ -149,7 +150,6 @@ MEDIA_URL = 'media/'
 
 MEDIA_ROOT = BASE_DIR / 'media'
 
-import os
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
